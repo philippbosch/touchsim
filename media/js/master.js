15eda4ac91750e2a6f21f0418971d464e7e0fc3a
Philipp Bosch <pb@philippbosch.de>
2010-04-14 00:13:10 +0200

    First try on GitHub page for touchsim.

---
+++ media/js/master.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+    $('#type-statusbar').bind('change', function() {
+        $('#statusbar').attr('class', $(this).val());
+    });
+    $('#toggle-locationbar').bind('change', function() {
+        $('body').toggleClass('locationbar', $(this).attr('checked'));
+    });
+    $('#toggle-toolbar').bind('change', function() {
+        $('body').toggleClass('toolbar', $(this).attr('checked'));
+    });
+    $('#toggle-device').bind('change', function() {
+        $('body').toggleClass('device', $(this).attr('checked'));
+    });
+    $('#carrier-text').bind('change keyup', function() {
+        $('#carrier').text($(this).val());
+    });
+    $('#location').bind('keyup reload', function(event) {
+        if ((event.keyCode == 13) || (event.type == 'reload')) {
+            var url = $(this).val();
+            console.log(url.substr(0,7));
+            if (url.substr(0,7) != 'http://' && url.substr(0,7) != 'file://') url = 'http://' + url;
+            $('#canvas').attr('src', url);
+            $(this).blur();
+        }
+    });
+    
+    $('input,select').live('change keyup', function() {
+        $('input,select').each(function() {
+            var value;
+            if ($(this).is('[type="checkbox"]')) {
+                value = $(this).attr('checked');
+            } else {
+                value = $(this).val();
+            }
+            localStorage.setItem($(this).attr('id'), value);
+        });
+    }).each(function() {
+        var value = localStorage.getItem($(this).attr('id'));
+        if ($(this).is('[type="checkbox"]')) {
+            $(this).attr('checked', value == 'true');
+        } else {
+            $(this).val(value);
+        }
+        $(this).triggerHandler('change');
+        $(this).triggerHandler('reload');
+    });
+    
+    $('#controlpanel form').bind('submit', function(e) {
+        e.preventDefault();
+    });
+});
